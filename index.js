d47cd98d34a361d1de340514e37bb934fa452875
naynapashilkar <nayna.pashilkar@gmail.com>
2018-07-26 21:01:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your code in this file!
-scuberGreetingForFeet(ride){
+function scuberGreetingForFeet(ride){
   if (ride <= 400) {
     console.log("This one is on me!")
   }
+  else if (ride <=)
 }
