9b8142813c8e0a491b48accd7b8ea608cfc3f2a7
naynapashilkar <nayna.pashilkar@gmail.com>
2018-07-26 21:11:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,4 +20,8 @@ function ternaryCheckCity (city){
   }
 }
 
-function switchOnCharmFromTip ()
+function switchOnCharmFromTip (tip){
+  if (tip == "generous"){
+    return "Thank you so much!"
+  }
+}
