5ec19c37a04f5682f4195d670d2efd423d9a4ee9
naynapashilkar <nayna.pashilkar@gmail.com>
2018-07-26 21:18:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return("This one is on me!")
+  }
+  else if (ride > 2000 && ride < 2500 ) {
+    return("I will gladly take your thirty bucks.")
+  }
+  else if (ride >= 2500) {
+    return("No can do.")
+  }
+}
+
+function ternaryCheckCity (city){
+  if (city == "NYC") {
+    return ("Ok, sounds good.")
+  }
+  else {
+    return "No go."
+  }
+}
+
+function switchOnCharmFromTip (tip){
+  if (tip == "generous"){
+    return "Thank you so much!"
+  }
+}
